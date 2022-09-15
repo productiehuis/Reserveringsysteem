9e3d9bf0b99afc9870676e6cc9372fee1d22ce3d
Kaine van Ommeren <kainevommeren@gmail.com>
2022-09-15 13:53:13 +0200

    added index file

--- public/index.php
+++ public/index.php
@@ -10,9 +10,24 @@ $test = "200";
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="./node_modules/bootstrap/dist/css/bootstrap-grid.css" rel="stylesheet">
+        <script src="./node_modules/bootstrap/dist/js/bootstrap.js"></script>
         <title>test</title>
     </head>
     <body>
         <p>lorem ipsum dolor sit amet</p>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col">
+                    Column
+                </div>
+                <div class="col">
+                    Column
+                </div>
+                <div class="col">
+                    Column
+                </div>
+            </div>
+        </div>
     </body>
 </html>
