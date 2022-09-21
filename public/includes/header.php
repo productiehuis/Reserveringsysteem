d7ad50cb33ad65d046ecf550d1e2bdfe19eeae5d
Kaine van Ommeren <kainevommeren@gmail.com>
2022-09-21 11:54:50 +0200

    [+] UI
    [+] Fixed redirection
    [+] Include header & footer from other files.

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,32 +1,45 @@
 <?php
 ## KW1C Blauw Hexcode
 $color = "#0E85C7";
-
-echo '
-<nav class="navbar navbar-expand-lg bg-gray-100 p-2">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-    Logo KW1C
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
-    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
-    aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Agenda</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Archief</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Zoeken" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Zoeken</button>
-      </form>
-    </div>
-  </div>
-</nav>
-';
+?>
+<!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="./css/bootstrap.min.css" rel="stylesheet">
+        <script src="./js/bootstrap.bundle.js" defer></script>
+        <title>KW1C Reserveringssysteem</title>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg bg-gray-100 p-2">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                Logo KW1C
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+                aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center">
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#">Agenda</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#">Archief</a>
+                    </li>
+                  </ul>
+                  <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Zoeken" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Zoeken</button>
+                  </form>
+                </div>
+              </div>
+            </nav>
+        </header>
+        <!-- Zie footer.php
--- public/index.php
+++ public/index.php
@@ -1,28 +1,20 @@
-<?php
-
-$test = "200";
-
-?>
-<!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <script src="js/bootstrap.bundle.js" defer></script>
-        <title>KW1C Reserveringssysteem</title>
-    </head>
-    <body>
-        <header>
-            <?php include "./includes/header.php"?>
-        </header>
+<?php include "./includes/header.php"?>
         <main>
-
+            <table class="table">
+                <thead>
+                    <th scope="col">Lorem</th>
+                    <th scope="col">ipsum</th>
+                </thead>
+                <tbody>
+                    <tr class="table-default">
+                        <th scope="row">1</th>
+                        <td>Kaine</td>
+                    </tr>
+                    <tr class="table-info">
+                        <th scope="row">2</th>
+                        <td>Kaine</td>
+                    </tr>
+                </tbody>
+            </table>
         </main>
-        <footer>
-            <?php include "./includes/footer.php"?>
-        </footer>
-    </body>
-</html>
+<?php include "./includes/footer.php"?>
