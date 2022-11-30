689705b46120966ba800c9a3350e2f7d19c49635
Ian <ianli2001@outlook.com>
2022-11-30 11:54:00 +0100

    Deployment test

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
-<?php include "./includes/header.php"; ?>
-        <main>
-            <div class="container">
-                <?php include "./includes/card.php"?>
-            </div>
-        </main>
-<?php include "./includes/footer.php"?>
+<?php include "public/index.php"; ?>
