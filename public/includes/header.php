e8bdc2fb5b1c3102035fcd0f88072d0935d290f8
Ian <ianli2001@outlook.com>
2022-11-30 12:17:25 +0100

    Reference test

--- public/includes/header.php
+++ public/includes/header.php
@@ -7,8 +7,8 @@ session_start();
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="css/bootstrap.css" rel="stylesheet">
-        <link href="css/index-addon.css" rel="stylesheet">
+        <link href="public/css/bootstrap.css" rel="stylesheet">
+        <link href="public/css/index-addon.css" rel="stylesheet">
         <title>KW1C Reserveringssysteem</title>
     </head>
     <body>
