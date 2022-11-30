f1bc8df4aea37bdebf17b502dbb5668f768f9514
Ian <ianli2001@outlook.com>
2022-11-30 14:41:33 +0100

    Connection fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php include "includes/header.php"; ?>
+<?php include "./includes/header.php"; ?>
         <main>
             <div class="container">
-                <?php include "includes/card.php" ?>
+                <?php include "./includes/card.php"?>
             </div>
         </main>
-<?php include "includes/footer.php" ?>
+<?php include "./includes/footer.php"?>
